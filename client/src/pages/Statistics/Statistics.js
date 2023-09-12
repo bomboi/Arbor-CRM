815fe820a4681a79174e1a42f6cd847606057d20
bomatix <matijabojovic@outlook.com>
2023-09-12 16:52:48 +0200

    add statistics

--- client/src/pages/Statistics/Statistics.js
+++ client/src/pages/Statistics/Statistics.js
@@ -6,6 +6,22 @@ import { Bar } from 'react-chartjs-2';
 import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
 import Axios from 'axios';
 import { useState } from 'react';
+import { isBrowser, isMobile, BrowserView, MobileView } from 'react-device-detect';
+
+const meseci = {
+    1: "Januar",
+    2: "Februar",
+    3: "Mart",
+    4: "April",
+    5: "Maj",
+    6: "Jun",
+    7: "Jul",
+    8: "Avgust",
+    9: "Septembar",
+    10: "Oktobar",
+    11: "Novembar",
+    12: "Decembar"
+}
 
 
 const Statistics = () => {
@@ -29,54 +45,122 @@ const Statistics = () => {
                 title="Statistika"
                 className="mb-3"
                 extra={[]}/>
-            <Row gutter={[20]}>
-                <Col span={12}>
-                    <Card>
-                        <Title level={4}>Promet</Title>
-                        <Row gutter={[10]}>
-                            <Col span={8}>
+            <BrowserView>
+                <Row gutter={[20]}>
+                    <Col span={12}>
+                        <Card>
+                            <Title level={4}>Promet</Title>
+                            <Row gutter={[10]}>
+                                <Col span={8}>
+                                    <Statistic
+                                    title="Trenutni mesec"
+                                    value={data.orderPrice}
+                                    precision={2}
+                                    valueStyle={{ color: thisMonthHigherPrice ? '#3f8600' : '#d10f0f'}}
+                                    prefix={thisMonthHigherPrice ? <ArrowUpOutlined /> : <ArrowDownOutlined/>}
+                                    suffix="RSD"/>
+                                </Col>
+                                <Col span={8}>
+                                    <Statistic
+                                    title="Prošli mesec"
+                                    value={data.orderPriceLastMonth}
+                                    precision={2}
+                                    valueStyle={{ color: '#999' }}
+                                    suffix="RSD"/>
+                                </Col>
+                            </Row>
+                        </Card>
+                    </Col>
+                    <Col span={12}>
+                        <Card>
+                            <Title level={4}>Broj porudžbina</Title>
+                            <Row gutter={[10]}>
+                                <Col span={8}>
+                                    <Statistic
+                                    title="Trenutni mesec"
+                                    value={data.orderCount}
+                                    precision={0}
+                                    valueStyle={{ color: thisMonthHigherPrice ? '#3f8600' : '#d10f0f' }}
+                                    prefix={thisMonthHigherPrice ? <ArrowUpOutlined /> : <ArrowDownOutlined/>}/>
+                                </Col>
+                                <Col span={8}>
+                                    <Statistic
+                                    title="Prošli mesec"
+                                    value={data.orderCountLastMonth}
+                                    precision={0}
+                                    valueStyle={{ color: '#999' }}/>
+                                </Col>
+                            </Row>
+                        </Card>
+                    </Col>
+                </Row>
+            </BrowserView>
+            <MobileView>
+                <Card>
+                    <Title level={4}>Promet</Title>
+                    <Row gutter={[10]}>
+                        <Col span={12}>
+                            <Statistic
+                            title="Trenutni mesec"
+                            value={data.orderPrice}
+                            precision={2}
+                            valueStyle={{ color: thisMonthHigherPrice ? '#3f8600' : '#d10f0f'}}
+                            prefix={thisMonthHigherPrice ? <ArrowUpOutlined /> : <ArrowDownOutlined/>}
+                            suffix="RSD"/>
+                        </Col>
+                        <Col span={12}>
+                            <Statistic
+                            title="Prošli mesec"
+                            value={data.orderPriceLastMonth}
+                            precision={2}
+                            valueStyle={{ color: '#999' }}
+                            suffix="RSD"/>
+                        </Col>
+                    </Row>
+                </Card>
+                <Card>
+                    <Title level={4}>Broj porudžbina</Title>
+                    <Row gutter={[10]}>
+                        <Col span={12}>
+                            <Statistic
+                            title="Trenutni mesec"
+                            value={data.orderCount}
+                            precision={0}
+                            valueStyle={{ color: thisMonthHigherPrice ? '#3f8600' : '#d10f0f' }}
+                            prefix={thisMonthHigherPrice ? <ArrowUpOutlined /> : <ArrowDownOutlined/>}/>
+                        </Col>
+                        <Col span={12}>
+                            <Statistic
+                            title="Prošli mesec"
+                            value={data.orderCountLastMonth}
+                            precision={0}
+                            valueStyle={{ color: '#999' }}/>
+                        </Col>
+                    </Row>
+                </Card>
+                <Card>
+                    <Title level={4}>Porudzbine po mesecima</Title>
+                    {data.ordersPerMonth && data.ordersPerMonth.map(orderData => 
+                    <>
+                        <div><b>{meseci[orderData._id.mesec]} {orderData._id.godina}</b></div>
+                        <Row>
+                            <Col span={12}>
                                 <Statistic
-                                title="Trenutni mesec"
-                                value={data.orderPrice}
-                                precision={2}
-                                valueStyle={{ color: thisMonthHigherPrice ? '#3f8600' : '#d10f0f'}}
-                                prefix={thisMonthHigherPrice ? <ArrowUpOutlined /> : <ArrowDownOutlined/>}
-                                suffix="RSD"/>
+                                value={orderData.brPorudz}
+                                valueStyle={{ color: '#999' }}
+                                precision={0}/>
                             </Col>
-                            <Col span={8}>
+                            <Col span={12}>
                                 <Statistic
-                                title="Prošli mesec"
-                                value={data.orderPriceLastMonth}
-                                precision={2}
+                                value={orderData.promet}
+                                precision={0}
                                 valueStyle={{ color: '#999' }}
                                 suffix="RSD"/>
                             </Col>
                         </Row>
-                    </Card>
-                </Col>
-                <Col span={12}>
-                    <Card>
-                        <Title level={4}>Broj porudžbina</Title>
-                        <Row gutter={[10]}>
-                            <Col span={8}>
-                                <Statistic
-                                title="Trenutni mesec"
-                                value={data.orderCount}
-                                precision={0}
-                                valueStyle={{ color: thisMonthHigherPrice ? '#3f8600' : '#d10f0f' }}
-                                prefix={thisMonthHigherPrice ? <ArrowUpOutlined /> : <ArrowDownOutlined/>}/>
-                            </Col>
-                            <Col span={8}>
-                                <Statistic
-                                title="Prošli mesec"
-                                value={data.orderCountLastMonth}
-                                precision={0}
-                                valueStyle={{ color: '#999' }}/>
-                            </Col>
-                        </Row>
-                    </Card>
-                </Col>
-            </Row>
+                    </>)}
+                </Card>
+            </MobileView>
         </div>
     )
 }
--- routes/statistics.js
+++ routes/statistics.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
 const isAuthenticated = require('../routes/auth').isAuthenticated;
 const Order = require('../models/Order');
+const date_fns = require('date-fns');
 
 router.use(isAuthenticated);
 
 router.get('/get', async (req, res) => {
     let today = new Date();
     let beginning = new Date(today.getFullYear(), today.getMonth(), 1);
-    let prevMonthBeginning = new Date(today.getFullYear(), today.getMonth() - 1, 1);
+    let prevMonthBeginning = date_fns.subMonths(beginning, 1);
+    let twoYearsAgo = date_fns.subYears(beginning, 2);
 
     const orderCount = await Order.aggregate([
         { 
@@ -49,38 +51,39 @@ router.get('/get', async (req, res) => {
         }
     ]).exec();
 
+    const ordersPerMonth = await Order.aggregate([
+        { 
+            $match: {
+                $expr: {
+                    $and: [
+                        { $gt: ['$latestVersionDate', twoYearsAgo] },
+                        { $lt: ['$latestVersionDate', today] }
+                    ]
+                }
+            }
+        },
+        {
+            $group: {
+                _id: { mesec: { $month: '$latestVersionDate'}, godina: { $year: '$latestVersionDate' }},
+                brPorudz: { $sum: 1 },
+                promet: { $sum: '$totalAmount' }
+            }
+        },
+        {
+            $sort: { "_id.godina": 1, "_id.mesec": 1 }
+        }
+    ]).exec();
+    
+    console.log(orderCountLastMonth)
     res.status(200).send({
         orderCount: orderCount.length == 0 ? 0 : orderCount[0].total,
         orderPrice: orderCount.length == 0 ? 0 : orderCount[0].price,
         orderCountLastMonth: orderCountLastMonth.length == 0 ? 0 : orderCountLastMonth[0].total,
-        orderPriceLastMonth: orderCountLastMonth.length == 0 ? 0 : orderCountLastMonth[0].price
+        orderPriceLastMonth: orderCountLastMonth.length == 0 ? 0 : orderCountLastMonth[0].price,
+        ordersPerMonth: ordersPerMonth
     });
 })
 
-// const orderCountLastMonth = await Order.aggregate([
-//     { 
-//         $match: {
-//             $expr: {
-//                 $and: [
-//                     { $gt: ['$latestVersionDate', prevMonthBeginning] },
-//                     { $lt: ['$latestVersionDate', today] }
-//                 ]
-//             }
-//         }
-//     },
-//     {
-//         $group: {
-//             _id: { mesec: { $month: '$latestVersionDate'}, godina: { $year: '$latestVersionDate' }},
-//             brPorudz: { $sum: 1 },
-//             promet: { $sum: '$totalAmount' }
-//         }
-//     },
-//     {
-//         $sort: { "_id.godina": 1, "_id.mesec": 1 }
-//     }
-// ]).exec();
-
-// console.log(orderCountLastMonth)
 
 
 module.exports = router;
